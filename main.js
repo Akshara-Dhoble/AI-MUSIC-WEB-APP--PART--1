bac4717bf4f8f2593071c1192ca3d4002b4d4c8a
Akshara-Dhoble <88190894+Akshara-Dhoble@users.noreply.github.com>
2022-05-15 13:47:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,15 +1,20 @@
+leftWrist = 0 ;
 song ="" ;
 song2 = "" ;
 leftWrist_x = 0 ;
 leftWrist_y = 0 ;
 rightWrist_x = 0 ;
 rightWrist_y = 0 ;
+    
+
 
 function setup() {
     canvas = createCanvas(600, 500);
     canvas.center();
+
     video = createCapture(VIDEO);
     video.hide();
+    
     poseNet = ml5.poseNet(video, modelLoaded);
     poseNet.on("pose", gotPoses);
 }   
@@ -32,17 +37,19 @@ function gotPoses(results){
 
 function draw(){
     image(video , 0 , 0 , 600 , 500);
+    
 }
 
 function preload(){
     song = loadSound("music.mp3");
     song2 = loadSound("musyc2.mp3");
 }
-function play(){
-    song.play();
-    song2.play();
-    song.setVolume(1);
-    song.rate(1);
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
 }
+
 
 
